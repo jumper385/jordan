5a0cb5bb7007a2013a5ed44bbf14837bc2af669b
Henry Chen <henrychen385@gmail.com>
2021-03-12 00:16:37 +0800

    added inventory_partnumber relationships

--- web/src/helpers/db/schemas.js
+++ web/src/helpers/db/schemas.js
@@ -19,3 +19,15 @@ export const Project_Inventory = mongoose.model('project_inventory', {
     inventory: { type: String, required: true },
     count: { type: Number, required: true },
 })
+
+export const Inventory_PartNumber = mongoose.model('inventory_partnumber', {
+    inventory: { type: String, required: true },
+    partnumber: { type: String, required: true }
+})
+
+export const PartNumber = mongoose.model('partnumber', {
+    id: { type: String, required: true},
+    pn: {type: String, required: true},
+    supplier: {type: String, required: true},
+    cost: {type: String, required: true},
+})
